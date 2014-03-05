0dcbb997857fe812a5fb9a624c249d29843d3c9d
Jichao Ouyang <oyanglulu@gmail.com>
2014-03-05 17:27:56 +0800

    bind click event

--- src/blogist.js
+++ src/blogist.js
@@ -1,4 +1,5 @@
-user=""
+var login_user;
+
 function getUser(){
 	return Q($.ajax({
 		url:"https://api.github.com/user"+ "?access_token=" + localStorage.getItem("access_token"),
@@ -9,21 +10,30 @@ function getUser(){
 function renderHeader(){
 	return getUser().then(function(user){
 		$(".navbar.navbar-default").html(nunjucks.render("src/templates/header.html",user));
-		user = user.login;
+		login_user = user.login;
 	});	
 }
 
 
-function getGist(){
+function getGists(){
 	return Q($.ajax({
-		url:"https://api.github.com/users/jcouyang/gists?access_token=" + localStorage.getItem("access_token"),
+		url:"https://api.github.com/users/"+login_user+"/gists?access_token=" + localStorage.getItem("access_token"),
 		method:'get'
 	}));
 };
 
 function renderGist(){
-	getGist().then(function(body){
-		$('.container .article').html(nunjucks.render("src/templates/article.html",{gists:body}));
+	getGists().then(function(body){
+		$('.container .article').html(nunjucks.render("src/templates/gistlist.html",{gists:body}));
+	}).then(bindEvent);
+}
+
+
+function bindEvent(){
+	$(".article li a").click(function(){
+		$.getJSON($(this).data("url")+".json?callback=?",function(data){
+			$(".article").html(data.div);
+		});
 	});
 }
 
